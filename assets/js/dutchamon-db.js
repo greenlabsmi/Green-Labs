d0323d1c74ea4855ba0caf7fa1336aeae0fc6620
greenlabsmi <shop@greenlabsmi.com>
2026-05-01 09:48:35 -0400

    Update and rename card-collection.js to dutchamon-db.js

---
+++ assets/js/dutchamon-db.js
@@ -0,0 +1,16 @@
+
+const GL_CARDS = [
+    { id: 'lemon-wookie', name: 'Lemon Wookie', image: 'assets/img/strains/lemon-wookie-art.jpg', reward: '$1 Off 1/8th', rarity: 'Indica-Hybrid' },
+    { id: 'falcon-9', name: 'Falcon 9', image: 'assets/img/strains/falcon-9-art.jpg', reward: '$1 Off 1/8th', rarity: 'Premium Hybrid' },
+    { id: 'solo-walker', name: 'Solo Walker', image: 'assets/img/strains/solo-walker-art.jpg', reward: '$1 Off 1/8th', rarity: 'Elite Genetics' },
+    { id: '13-layer-cake', name: '13 Layer Cake', image: 'assets/img/strains/13-layer-cake-art.jpg', reward: '$1 Off 1/8th', rarity: 'Sativa Hybrid' },
+    { id: 'banana-split', name: 'Banana Split', image: 'assets/img/strains/banana-split-art.jpg', reward: '$1 Off 1/8th', rarity: 'Hybrid' },
+    { id: 'double-bubble', name: 'Double Bubble', image: 'assets/img/strains/double-bubble-art.jpg', reward: '$1 Off 1/8th', rarity: 'Hybrid' },
+    { id: 'security-guy', name: 'The Bodyguard', image: 'assets/img/hero.jpg', reward: 'Free Sticker', rarity: 'Secret Drop' },
+    { id: 'colossus', name: 'The Colossus', image: 'assets/img/greenlabs-logo.png', reward: '15% Off Total', rarity: 'Legendary' }
+];
+
+const GL_SETS = [
+    { id: 'jedi', name: 'Jedi Council', reward: 'Reward: 20% Off Set', cardIds: ['lemon-wookie', 'falcon-9', 'solo-walker'] },
+    { id: 'bakery', name: 'The Bakery', reward: 'Reward: Free Edible', cardIds: ['13-layer-cake', 'banana-split', 'double-bubble'] }
+];
